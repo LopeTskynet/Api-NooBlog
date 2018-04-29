1e9ee4adc7c0ad715cf5b57bf17b4c01b74cefb3
Loperiane <loperanes@gmail.com>
2018-04-30 01:09:30 +0200

    add the create method on user

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,7 +1,30 @@
 const Users = require('../models/users.models')
 
 exports.create = (req,res) => {
+    
+    if(!req.body.pseudo) {
+        return res.status(400).send({
+            message: "no pseudo given"
+        })
+    }
 
+    const users = new Users({
+        pseudo: req.body.title,
+        password: req.body.password,
+        role: "guest",
+        first_name: req.body.first_name || null,
+        last_name: req.body.last_name ||null,
+        email: req.body.email
+    })
+
+    users.save()
+    .then(data => {
+        res.send(data)
+    }).catch(err =>{
+        res.status(500).send({
+            message: err.message ||"an error occured."
+        })
+    })
 }
 
 exports.findAll = (req,res) => {
@@ -17,5 +40,5 @@ exports.update = (req,res) => {
 }
 
 exports.delete = (req,res) => {
-    
+
 }
