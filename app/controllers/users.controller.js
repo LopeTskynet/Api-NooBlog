2dac7675d40f0b85abf9ec25fe7fd143e57de171
Loperiane <loperanes@gmail.com>
2018-04-30 00:51:02 +0200

    implementing controllers

---
+++ app/controllers/users.controller.js
@@ -0,0 +1,21 @@
+const Users = require('../models/users.models')
+
+exports.create = (req,res) => {
+
+}
+
+exports.findAll = (req,res) => {
+
+}
+
+exports.findOne = (req,res) => {
+
+}
+
+exports.update = (req,res) => {
+
+}
+
+exports.delete = (req,res) => {
+    
+}
