ab54969058a5babed7d05152a10a9fd5be255f06
Loperiane <loperanes@gmail.com>
2018-04-30 01:08:43 +0200

    change naming

--- app/models/users.models.js
+++ app/models/users.models.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const UsersSchema = mongoose.Schema({
-    Pseudo: String,
-    Password: String,
-    Role: String,
-    First_name: String,
-    Last_name: String,
-    Email: String
+    pseudo: String,
+    password: String,
+    role: String,
+    first_name: String,
+    last_name: String,
+    email: String
 })
 
 module.exports = mongoose.model('Users', UsersSchema)
