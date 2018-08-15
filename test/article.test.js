73fbace61a4636a7ecf1fac7aa3e236d849ce00b
DylanBourdere <loperanes@gmail.com>
2018-08-15 21:43:36 +0200

    Add test for article

--- test/article.test.js
+++ test/article.test.js
@@ -6,6 +6,8 @@ let chaiHttp = require('chai-http')
 let server = require('../index.js')
 let should = chai.should()
 
+const jwt = require('jsonwebtoken')
+
 chai.use(chaiHttp)
 
 describe('Article', () => {
@@ -51,6 +53,150 @@ describe('Article', () => {
           })
       })
     })
+  })
+
+  describe('POST /article', () => {
+    it('it should not POST an article without title', (done) => {
+      let date = new Date().toLocaleDateString()
+      let article = {
+        pseudo: 'loperiane',
+        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJub29ibG9nIiwic3ViIjoidGVzdCIsInNjb3BlIjoic2VsZiIsImp0aSI6IjU5MjFkMDA5LWNlYjQtNDhlOC1iMjJhLTk4ZmQ2MjM3MDRmZSIsImlhdCI6MTUzNDM0Mzg0MCwiZXhwIjoxNTM0MzQ3NDQwfQ.ndvYjhtJk5qwPxsiuxH2FaJKnRoX7vmvvTeW5Ny6RsQ',
+        article: 'Article test',
+        tag: 'OK',
+        date: date,
+        isFinish: true
+      }
+      chai.request(server)
+      .post('/api/v1/article/create')
+      .send(article)
+      .end((err, res) => {
+        res.should.have.status(400)
+        res.body.should.have.property('message').eql('no title given')
+        done()
+      })
+    })
+
+    it('it should not POST an article without article', (done) => {
+      let date = new Date().toLocaleDateString()
+      let article = {
+        pseudo: 'loperiane',
+        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJub29ibG9nIiwic3ViIjoidGVzdCIsInNjb3BlIjoic2VsZiIsImp0aSI6IjU5MjFkMDA5LWNlYjQtNDhlOC1iMjJhLTk4ZmQ2MjM3MDRmZSIsImlhdCI6MTUzNDM0Mzg0MCwiZXhwIjoxNTM0MzQ3NDQwfQ.ndvYjhtJk5qwPxsiuxH2FaJKnRoX7vmvvTeW5Ny6RsQ',
+        title: 'Article test',
+        tag: 'OK',
+        date: date,
+        isFinish: true
+      }
+      chai.request(server)
+      .post('/api/v1/article/create')
+      .send(article)
+      .end((err, res) => {
+        res.should.have.status(400)
+        res.body.should.have.property('message').eql('no article given')
+        done()
+      })
+    })
+
+    it('it should not POST an article without tag', (done) => {
+      let date = new Date().toLocaleDateString()
+      let article = {
+        pseudo: 'loperiane',
+        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJub29ibG9nIiwic3ViIjoidGVzdCIsInNjb3BlIjoic2VsZiIsImp0aSI6IjU5MjFkMDA5LWNlYjQtNDhlOC1iMjJhLTk4ZmQ2MjM3MDRmZSIsImlhdCI6MTUzNDM0Mzg0MCwiZXhwIjoxNTM0MzQ3NDQwfQ.ndvYjhtJk5qwPxsiuxH2FaJKnRoX7vmvvTeW5Ny6RsQ',
+        article: 'Article test',
+        title: 'OK',
+        date: date,
+        isFinish: true
+      }
+      chai.request(server)
+      .post('/api/v1/article/create')
+      .send(article)
+      .end((err, res) => {
+        res.should.have.status(400)
+        res.body.should.have.property('message').eql('no tag given')
+        done()
+      })
+    })
+
+    it('it should not POST an article without isFinish', (done) => {
+      let date = new Date().toLocaleDateString()
+      let article = {
+        pseudo: 'loperiane',
+        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJub29ibG9nIiwic3ViIjoidGVzdCIsInNjb3BlIjoic2VsZiIsImp0aSI6IjU5MjFkMDA5LWNlYjQtNDhlOC1iMjJhLTk4ZmQ2MjM3MDRmZSIsImlhdCI6MTUzNDM0Mzg0MCwiZXhwIjoxNTM0MzQ3NDQwfQ.ndvYjhtJk5qwPxsiuxH2FaJKnRoX7vmvvTeW5Ny6RsQ',
+        article: 'Article test',
+        title: 'ok',
+        tag: 'OK',
+        date: date
+      }
+      chai.request(server)
+      .post('/api/v1/article/create')
+      .send(article)
+      .end((err, res) => {
+        res.should.have.status(400)
+        res.body.should.have.property('message').eql('no state given')
+        done()
+      })
+    })
+
+    it('it should not POST an article without pseudo', (done) => {
+      let date = new Date().toLocaleDateString()
+      let article = {
+        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJub29ibG9nIiwic3ViIjoidGVzdCIsInNjb3BlIjoic2VsZiIsImp0aSI6IjU5MjFkMDA5LWNlYjQtNDhlOC1iMjJhLTk4ZmQ2MjM3MDRmZSIsImlhdCI6MTUzNDM0Mzg0MCwiZXhwIjoxNTM0MzQ3NDQwfQ.ndvYjhtJk5qwPxsiuxH2FaJKnRoX7vmvvTeW5Ny6RsQ',
+        article: 'Article test',
+        title: 'ok',
+        tag: 'OK',
+        date: date,
+        isFinish: true
+      }
+      chai.request(server)
+      .post('/api/v1/article/create')
+      .send(article)
+      .end((err, res) => {
+        res.should.have.status(400)
+        res.body.should.have.property('message').eql('no pseudo given')
+        done()
+      })
+    })
+
+    it('it should not POST an article without token', (done) => {
+      let date = new Date().toLocaleDateString()
+      let article = {
+        pseudo: 'loperiane',
+        article: 'Article test',
+        title: 'ok',
+        tag: 'OK',
+        date: date,
+        isFinish: true
+      }
+      chai.request(server)
+      .post('/api/v1/article/create')
+      .send(article)
+      .end((err, res) => {
+        res.should.have.status(400)
+        res.body.should.have.property('message').eql('no token given')
+        done()
+      })
+    })
+
+    it('it should POST an article', (done) => {
+      let date = new Date().toLocaleDateString()
+      let article = {
+        pseudo: 'loperiane',
+        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJub29ibG9nIiwic3ViIjoidGVzdCIsInNjb3BlIjoic2VsZiIsImp0aSI6ImYzYWFjYzZlLWRiMjUtNGJkZi1iNTEwLTliMTQyZDI2MzU5NSIsImlhdCI6MTUzNDM2MDQ3MCwiZXhwIjoxNTM0MzY0MDcwfQ.8c9UKTu0dH-zndKRGi0SqbM9pPtw9lQoS1zFHl6FuzA',
+        article: 'Article test',
+        title: 'ok',
+        tag: 'OK',
+        date: date,
+        isFinish: 'true'
+      }
+      chai.request(server)
+      .post('/api/v1/article/create')
+      .send(article)
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        done()
+      })
+    })
+
   })
 
 })
