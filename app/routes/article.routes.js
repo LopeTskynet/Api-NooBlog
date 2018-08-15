49fb3fa01aa9452ad1b3f15adf89ea6af5933b56
Loperiane <loperanes@gmail.com>
2018-08-15 19:30:05 +0200

    modif on article router

--- app/routes/article.routes.js
+++ app/routes/article.routes.js
@@ -2,5 +2,6 @@ module.exports = (app) => {
     const article = require('../controllers/article.controller.js')
     //Router linked to the controller
     app.post('/api/v1/article/create', article.create)
-    app.post('/api/v1/article/all', article.findAll)
+    app.get('/api/v1/article/all', article.findAll)
+    app.get('/api/v1/article/:articleId', article.findOne)
 }
