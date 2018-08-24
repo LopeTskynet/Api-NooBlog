73be052b769cef621446a3865c2265d2cc275bab
DylanBourdere <loperanes@gmail.com>
2018-08-24 22:25:14 +0200

    fix eslint users routes

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-    const users = require('../controllers/users.controller.js')
-    //Router linked to the controller
-    app.post('/api/v1/users', users.create)
-    app.post('/api/v1/users/connection', users.connection)
-    app.post('/api/v1/users/isconnected', users.tokenVerify)
-    app.get('/api/v1/users', users.findAll)
-    app.get('/api/v1/users/:usersId', users.findOne)
-    app.put('/api/v1/users/update/', users.update)
-    app.delete('/api/v1/users/:usersId', users.delete)
+  const users = require('../controllers/users.controller.js')
+  // Router linked to the controller
+  app.post('/api/v1/users', users.create)
+  app.post('/api/v1/users/connection', users.connection)
+  app.post('/api/v1/users/isconnected', users.tokenVerify)
+  app.get('/api/v1/users', users.findAll)
+  app.get('/api/v1/users/:usersId', users.findOne)
+  app.put('/api/v1/users/update/', users.update)
+  app.delete('/api/v1/users/:usersId', users.delete)
 }
