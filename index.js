43e3b447296c9c61d82f0fcd776c2f4ffb86e1bd
DylanBourdere <loperanes@gmail.com>
2018-08-24 22:26:11 +0200

    fix eslint index.js

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-//replace mongoose promise system with bluebird promise
-const Promise = require("bluebird")
+// replace mongoose promise system with bluebird promise
+const Promise = require('bluebird')
 mongoose.Promise = Promise
 
 var cors = require('cors')
 
-const logger = require("./app/logs")
-//Config files
+const logger = require('./app/logs')
+// Config files
 const dbConfig = require('./config/database.conf')
 const config = require('./config/server.conf')
 
-//create express app
+// create express app
 const app = express()
 
-//parse request application/x-www-form-urlencoded
+// parse request application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
 
-//parse request application/json
+// parse request application/json
 app.use(bodyParser.json())
 
-//use cors for allow access on requests
+// use cors for allow access on requests
 app.use(cors())
-//router
+// router
 app.get('/', (req,res) => {
-  res.json({"Message": "Welcome on the NooBlog API"})
+  res.json({'Message': 'Welcome on the NooBlog API'})
 })
 
 require('./app/routes/users.routes')(app)
 require('./app/routes/article.routes')(app)
 require('./app/routes/technicalsheet.routes')(app)
-//launch
+// launch
 app.listen(config.port, () => {
-    console.log("Server on port " + config.port)
+  console.log('Server on port ' + config.port)
 })
 
 mongoose.connect(dbConfig.url)
   .then(() => {
-    console.log("Connected to the database")
+    console.log('Connected to the database')
   }).catch(err => {
-    console.log("Cannot connect to the database. Exiting now...")
+    console.log('Cannot connect to the database. Exiting now...')
     process.exit()
   })
 
