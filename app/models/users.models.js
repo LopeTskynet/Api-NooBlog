0c78a580e9d0777fa90d7993ce5c15ecc531914b
DylanBourdere <loperanes@gmail.com>
2018-08-24 22:23:19 +0200

    fix eslint users models

--- app/models/users.models.js
+++ app/models/users.models.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-//Schema of the database
+// Schema of the database
 const UsersSchema = mongoose.Schema({
-    pseudo: String,
-    password: String,
-    role: String,
-    first_name: String,
-    last_name: String,
-    email: String,
-    token: Object
+  pseudo: String,
+  password: String,
+  role: String,
+  first_name: String,
+  last_name: String,
+  email: String,
+  token: Object
 })
 
 module.exports = mongoose.model('Users', UsersSchema)
