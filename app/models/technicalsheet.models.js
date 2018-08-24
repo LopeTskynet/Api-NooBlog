6311ab96adeb04196003aa2829c6173dc355124a
DylanBourdere <loperanes@gmail.com>
2018-08-24 22:22:20 +0200

    fix eslint technical sheet models

--- app/models/technicalsheet.models.js
+++ app/models/technicalsheet.models.js
@@ -1,33 +1,33 @@
 const mongoose = require('mongoose')
 
-//Schema of the database
+// Schema of the database
 const TechnicalsheetSchema = mongoose.Schema({
-    name: String,
-    pharmacologie: String,
-    chimie: String,
-    toxicity: String,
-    isFinish: Boolean,
-    effects: {
-      physic: {
-        name: Array,
-        describe: Array,
-        counter: Number
-      },
-      cognitif: {
-        name: Array,
-        describe: Array,
-        counter: Number
-      },
-      indesirable: {
-        name: Array,
-        describe: Array,
-        counter: Number
-      }
+  name: String,
+  pharmacologie: String,
+  chimie: String,
+  toxicity: String,
+  isFinish: Boolean,
+  effects: {
+    physic: {
+      name: Array,
+      describe: Array,
+      counter: Number
     },
-    references: {
-      urlTab: Array,
+    cognitif: {
+      name: Array,
+      describe: Array,
+      counter: Number
+    },
+    indesirable: {
+      name: Array,
+      describe: Array,
       counter: Number
     }
+  },
+  references: {
+    urlTab: Array,
+    counter: Number
+  }
 })
 
 module.exports = mongoose.model('Technicalsheet', TechnicalsheetSchema)
