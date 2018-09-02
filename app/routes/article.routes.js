57b15d00da10f6ad418437ea36faf37b3d344e42
DylanBourdere <loperanes@gmail.com>
2018-09-02 15:27:48 +0200

    create get tags route

--- app/routes/article.routes.js
+++ app/routes/article.routes.js
@@ -1,8 +1,10 @@
 module.exports = (app) => {
+  const tags = require('../controllers/tag.controller.js')
   const article = require('../controllers/article.controller.js')
   // Router linked to the controller
   app.post('/api/v1/article/create', article.create)
   app.post('/api/v1/article/all', article.findAll)
   app.post('/api/v1/article/inprogress', article.findInProgress)
   app.get('/api/v1/article/:articleId', article.findOne)
+  app.get('/api/v1/tags', tags.list)
 }
