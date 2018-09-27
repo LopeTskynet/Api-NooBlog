1f95655df0a59827dfde4978dc011fe3682025da
tskynet <stephdumonceau@gmail.com>
2018-09-27 21:59:09 +0200

    fix the method create for the technicalsheet

--- app/controllers/technicalsheet.method.js
+++ app/controllers/technicalsheet.method.js
@@ -14,13 +14,39 @@ exports.checkTechnicalObject = (technicalSheet) => {
         if(technicalSheet.effects.physic && technicalSheet.effects.cognitif && technicalSheet.effects.indesirable){
           if(technicalSheet.effects.physic.name && technicalSheet.effects.cognitif.name && technicalSheet.effects.indesirable.name){
             //Clean the first data in tab, because we got '' all time.
-            technicalSheet.effects.physic.name.splice(0, 1)
-            technicalSheet.effects.cognitif.name.splice(0, 1)
-            technicalSheet.effects.indesirable.name.splice(0, 1)
-            technicalSheet.effects.physic.describe.splice(0, 1)
-            technicalSheet.effects.cognitif.describe.splice(0, 1)
-            technicalSheet.effects.indesirable.describe.splice(0, 1)
-            technicalSheet.references.urlTab.splice(0, 1)
+            if(technicalSheet.effects.physic.describe[0] == ''){
+              console.log('condition ok')
+            }
+            if(technicalSheet.effects.physic.describe[0] == undefined){
+              console.log('second if')
+            }
+            if(technicalSheet.effects.physic.describe[0] == undefined && technicalSheet.effects.physic.name[0] == undefined) {
+              console.log('if 1')
+              technicalSheet.effects.physic.describe.splice(0, 1)
+            }
+            if(technicalSheet.effects.cognitif.describe[0] == undefined && technicalSheet.effects.cognitif.name[0] == undefined){
+              console.log('if 2')
+              technicalSheet.effects.cognitif.describe.splice(0, 1)
+            }
+            if(technicalSheet.effects.indesirable.describe[0] == undefined && technicalSheet.effects.indesirable.name[0] == undefined){
+              console.log('if 3')
+              technicalSheet.effects.indesirable.describe.splice(0, 1)
+            }
+            if(technicalSheet.effects.physic.name[0] == undefined){
+              console.log('if4')
+              technicalSheet.effects.physic.name.splice(0, 1)
+            }
+            if(technicalSheet.effects.cognitif.name[0] == undefined){
+              console.log('if5')
+              technicalSheet.effects.cognitif.name.splice(0, 1)
+            }
+            if(technicalSheet.effects.indesirable.name[0] == undefined){
+              console.log('if6')
+              technicalSheet.effects.indesirable.name.splice(0, 1)
+            }
+            if(technicalSheet.references.urlTab[0] == undefined){
+              technicalSheet.references.urlTab.splice(0, 1)
+            }
             if(checkTab(technicalSheet.effects.physic.name) && checkTab(technicalSheet.effects.cognitif.name) && checkTab(technicalSheet.effects.indesirable.name)){
               if(checkTab(technicalSheet.references.urlTab)){
                 isComplete = true
@@ -57,6 +83,7 @@ exports.checkTechnicalObject = (technicalSheet) => {
 checkTab = (tab) => {
   bool = true
   tab.forEach( item => {
+    console.log(item)
     item ? 'ok tab in order' : bool = false
   })
   return bool
